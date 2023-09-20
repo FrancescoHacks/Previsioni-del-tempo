064571695162021b863db790f1574aceca3118ea
FrancescoHacks <e.lapera.francesco@gmail.com>
2023-09-20 13:36:08 +0200

    useless changes

--- Script/Script.js
+++ Script/Script.js
@@ -18,4 +18,11 @@ function createwMap(){
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+let marker = L.marker([latitudine, longitudine]).addTo(map);
+
+map.on("click", function(event){
+     marker.remove()
+     marker = L.marker([event.latlng.lat, event.latlng.lng]).addTo(map);
+})
 }
