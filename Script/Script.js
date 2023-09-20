b4cc69e282d0f8ead0415641f3b6475e6d342eed
FrancescoHacks <e.lapera.francesco@gmail.com>
2023-09-20 13:31:42 +0200

    risistemazione cartelle

--- Script/Script.js
+++ Script/Script.js
@@ -1,6 +1,8 @@
 let latitudine = 41.9027835
 let longitudine = 12.4963655
 
+
+
 navigator.geolocation.getCurrentPosition(function(event){
      console.log(event);
      latitudine = event.coords.latitude;
@@ -11,19 +13,9 @@ navigator.geolocation.getCurrentPosition(function(event){
 })
 
 function createwMap(){
-     let map = L.map('map').setView([latitudine, longitudine], 13);
+     var map = L.map('map').setView([latitudine, longitudine], 13);
      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
-let marker = L.marker([latitudine, longitudine]).addTo(map);
-
-map.on("click", function(event){
-     marker.remove();
-     marker = L.marker([event.latlng.lat, event.latlng.lng]).addTo(map);
-
-})
 }
-
-
